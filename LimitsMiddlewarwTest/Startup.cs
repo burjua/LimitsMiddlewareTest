1dc748d8239374e7a4ef6e72614c7f52a193f7cd
Zakhar <zzuban@palantirsolutions.com>
2015-09-03 10:30:17 +0100

    initial

---
+++ LimitsMiddlewarwTest/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LimitsMiddlewarwTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use().MaxUrlLength(2048); // Doesn't work, what shall I do? Missing usings? Which ones? 
+            
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
